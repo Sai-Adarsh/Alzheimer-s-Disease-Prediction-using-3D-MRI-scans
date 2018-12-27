d584f08c4dd925adb1fd453fef0e7c5879d90609
Sai Adarsh <sivakumar.sk11865.sk@gmail.com>
2018-12-27 20:02:06 +0530

    Add files via upload

---
+++ Code/imageProcessing.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+img = cv2.imread('1.png')
+#img = cv2.cvtColor(img, cv2.COLOR_BGR2 GRAY)
+_ ,thresh = cv2.threshold(img,28,255,cv2.THRESH_BINARY)
+thresh = cv2.cvtColor(thresh, cv2.COLOR_BGR2GRAY)
+n_white_pix = np.sum(thresh == 255)
+n_black_pix = np.sum(thresh == 0)
+print('Number of white pixels:[[', n_white_pix,']:[',n_black_pix, ']]' )
+cv2.imshow('thresh', thresh)
+cv2.waitKey(0)
